acf01a68c8e5d3f0b1defd223bec91947498eb2b
trocprgm <trocprgm@protonmail.com>
2022-01-04 09:58:00 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+these are just some of my personal scripts
+
+unlikely to be of any use to anyone other then me
---
+++ cleaner.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mv /home/adman/Amy/* /home/adman/Amy/arch
+
+mv /home/adman/Downloads/* /home/adman/Downstale/
