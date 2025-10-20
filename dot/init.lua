83048abe48d302456dcb9c8bf0661abe0e772f27
trocprgm <trocprgm@protonmail.com>
2025-10-20 07:57:15 -0700

    1. added init.lua to the tracking system

---
+++ dot/init.lua
@@ -0,0 +1,36 @@
+vim.cmd('source ~/.vimrc')
+
+local actions = require('telescope.actions')
+
+require('telescope').setup({
+  defaults = {
+    file_ignore_patterns = {
+          '^node_modules/', -- Ignore node_modules directory
+          '%.log',         -- Ignore files ending with .log
+          'build/',        -- Ignore build directory
+          '.git/',
+          '.cache/',
+        },
+    mappings = {
+      i = {
+        ["<C-_>"] = actions.move_selection_previous, -- make Ctrl-/ act like Ctrl-p
+        ["<C-p>"] = false,                            -- disable original Ctrl-p
+        ["?"] = false,                                -- stop Telescope help from opening on Ctrl-/
+        ["<F2>"] = actions.which_key
+      },
+      n = {
+        ["<C-_>"] = actions.move_selection_previous,  -- optional: same in normal mode of picker
+        ["<F2>"] = actions.which_key
+      },
+    },
+  },
+})
+
+require('telescope').setup({
+  pickers = {
+    find_files = {
+      hidden = true,
+      no_ignore = false
+    }
+  }
+})
