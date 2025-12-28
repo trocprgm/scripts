004ef016fb41a1064987500454632482c2b445cf
trocprgm <trocprgm@protonmail.com>
2025-12-28 04:17:38 -0800

    Transfered to Vim Wiki

--- sidebyside.sh
+++ sidebyside.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 xrandr --output DisplayPort-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --off --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal
+# feh --bg-fill /home/$USER/.config/bg2.png
